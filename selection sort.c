df39b86b7bac2e2bbdeed12d64052fe2eaaf06f7
sakshibaisla12 <113257162+sakshibaisla12@users.noreply.github.com>
2022-12-21 05:56:21 +0530

    Update selection sort.c

--- selection sort.c
+++ selection sort.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-int main(){
-int i,n,a[10];
-printf("enter the no");
-scanf("%d",&n);
-printf("enter the element of array");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-printf("%d",a[i]);
-}
+// pending
