e7ab51c0bf4e145abedff5061cfd407b8a39da20
sakshibaisla12 <113257162+sakshibaisla12@users.noreply.github.com>
2022-12-21 05:38:28 +0530

    Update add matrics.c

--- add matrics.c
+++ add matrics.c
@@ -1,8 +1,7 @@
+// add matrix 
 # include<stdio.h>
 int main(){
 int a[3][3],b[3][3],c[3][3],i,j;
-printf("NAME=sakshi\n");
-printf("Roll no =2100321530138\n");
 printf("enter your value in matrics ");
 for(i=0;i<=2;i++)
 for(j=0;j<=2;j++)
