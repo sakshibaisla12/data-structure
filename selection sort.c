70b7537d8f490d0afc6c8dc4ba85f1ed6f3c1762
sakshibaisla12 <113257162+sakshibaisla12@users.noreply.github.com>
2022-12-21 05:54:42 +0530

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int i,n,a[10];
+printf("enter the no");
+scanf("%d",&n);
+printf("enter the element of array");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("%d",a[i]);
+}
